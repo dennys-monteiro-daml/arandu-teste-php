ec5d4dedf4e42d8598c3667d0061c8b427e0c58d
Dennys Monteiro <dennyshagamenom@gmail.com>
2023-01-13 12:15:15 +0000

    Finalizado ambiente

--- public/js/app.js
+++ public/js/app.js
@@ -19437,8 +19437,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /mnt/97fb2f28-9f80-45b1-996a-6cffbde28b5a/www/arandu-teste-php/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /mnt/97fb2f28-9f80-45b1-996a-6cffbde28b5a/www/arandu-teste-php/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! /workspaces/arandu-teste-php/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /workspaces/arandu-teste-php/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
